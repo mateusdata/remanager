8de80795abae5b4daaa9b79d7b4f409077d93cdf
Mateus Santos <mateuspele2015@gmail.com>
2025-04-07 01:16:33 -0300

    features: added new features

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
+'use client'
+import axios from 'axios';
 import React from 'react';
 
-export default function Home() {
+interface Data {
+  message: string;
+  version: string;
+  details: string
+}
+export default async function Home() {
+  const response = await fetch("https://remanagerapi.vercel.app/");
+  const data: Data = await response.json();
+
   return (
     <div className="flex items-center justify-center h-screen ">
       <div className="text-center">
         <h1 className="text-4xl font-bold">Gestão de Revisão</h1>
-        <p className="mt-4 text-lg">Site em produção. Acompanhe as novidades em breve!</p>
+        <p className="mt-4 text-lg">Site em produçãos. Acompanhe as novidades em breve!</p>
+        <p>{data.message}</p>
+        <p>{data.version}</p>
+        <p>{data.details}</p>
       </div>
     </div>
   );
